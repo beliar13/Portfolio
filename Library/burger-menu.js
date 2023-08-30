a7556ad0be5d8165c4849a2ca1541d0da518eb68
Denis Sevostyanov <tesbeliar@gmail.com>
2023-08-30 20:57:25 +0300

    refactor: change about section && feat: add 4-5 buttons actions

--- Library/burger-menu.js
+++ Library/burger-menu.js
@@ -5,6 +5,8 @@ const WHOLE_PAGE = document.getElementById('main');
 const FIRST_BUTTON = document.getElementById('first-btn');
 const SECOND_BUTTON = document.getElementById('second-btn');
 const THIRD_BUTTON = document.getElementById('third-btn');
+const FOURTH_BUTTON = document.getElementById('fourth-btn');
+const FIFTH_BUTTON = document.getElementById('fifth-btn');
 const CAROUSEL = document.getElementById('about-carousel');
 
 
@@ -47,23 +49,47 @@ WHOLE_PAGE.addEventListener('click', (event) => {
 
 
 FIRST_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-end') & el.classList.add('to-the-left') & el.classList.add('to-the-begining'));
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-end') & el.classList.add('to-the-left') & el.classList.remove('to-the-fourth') & el.classList.remove('.to-the-fifth') & el.classList.add('to-the-begining'));
     SECOND_BUTTON.classList.remove('clicked-btn');
     THIRD_BUTTON.classList.remove('clicked-btn');
+    FOURTH_BUTTON.classList.remove('clicked-btn');
+    FIFTH_BUTTON.classList.remove('clicked-btn');
     FIRST_BUTTON.classList.add('clicked-btn');
 });
 
 SECOND_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-end') & el.classList.remove('to-the-begining') & el.classList.add('to-the-left'));
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-end') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-fourth') & el.classList.remove('.to-the-fifth') & el.classList.add('to-the-left'));
     THIRD_BUTTON.classList.remove('clicked-btn');
     FIRST_BUTTON.classList.remove('clicked-btn');
+    FOURTH_BUTTON.classList.remove('clicked-btn');
+    FIFTH_BUTTON.classList.remove('clicked-btn');
     SECOND_BUTTON.classList.add('clicked-btn');
 });
 
 
 THIRD_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.add('to-the-end'));
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-fourth') & el.classList.remove('.to-the-fifth') & el.classList.add('to-the-end'));
     SECOND_BUTTON.classList.remove('clicked-btn');
     FIRST_BUTTON.classList.remove('clicked-btn');
+    FOURTH_BUTTON.classList.remove('clicked-btn');
+    FIFTH_BUTTON.classList.remove('clicked-btn');
     THIRD_BUTTON.classList.add('clicked-btn');
+});
+
+FOURTH_BUTTON.addEventListener('click', (event) => {
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-end') & el.classList.remove('.to-the-fifth') & el.classList.add('to-the-fourth'));
+    SECOND_BUTTON.classList.remove('clicked-btn');
+    FIRST_BUTTON.classList.remove('clicked-btn');
+    THIRD_BUTTON.classList.remove('clicked-btn');
+    FIFTH_BUTTON.classList.remove('clicked-btn');
+    FOURTH_BUTTON.classList.add('clicked-btn');
+});
+
+FIFTH_BUTTON.addEventListener('click', (event) => {
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-end') & el.classList.remove('to-the-fourth') & el.classList.add('.to-the-fifth'));
+    SECOND_BUTTON.classList.remove('clicked-btn');
+    FIRST_BUTTON.classList.remove('clicked-btn');
+    THIRD_BUTTON.classList.remove('clicked-btn');
+    FOURTH_BUTTON.classList.remove('clicked-btn');
+    FIFTH_BUTTON.classList.add('clicked-btn');
 });
