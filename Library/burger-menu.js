42d54086f034f0b6f4d76a626dcaf44e5e90e5d0
Denis Sevostyanov <tesbeliar@gmail.com>
2023-08-31 21:27:44 +0300

    feat: add scripts for carrets

--- Library/burger-menu.js
+++ Library/burger-menu.js
@@ -2,15 +2,31 @@ const BURGER = document.getElementById('burger');
 const NAV_LINK = document.getElementById('navigation-area');
 const MENU_LIST = document.getElementById('nav-menu');
 const WHOLE_PAGE = document.getElementById('main');
+const BUTTONS_CONTAINER = document.getElementById('carousel-pagination');
 const FIRST_BUTTON = document.getElementById('first-btn');
 const SECOND_BUTTON = document.getElementById('second-btn');
 const THIRD_BUTTON = document.getElementById('third-btn');
 const FOURTH_BUTTON = document.getElementById('fourth-btn');
 const FIFTH_BUTTON = document.getElementById('fifth-btn');
 const CAROUSEL = document.getElementById('about-carousel');
+const LEFT_CARRET = document.getElementById('left-carret');
+const RIGHT_CARRET = document.getElementById('right-carret');
+let image_index = 0;
 
 
 MENU_LIST.querySelectorAll('li').forEach(el => el.classList.add('list-item-hidden'));
+
+const resetButtonsStyle = () => {
+    BUTTONS_CONTAINER.querySelectorAll('div').forEach(btn => btn.removeAttribute('class') & btn.setAttribute('class','carousel-pagination-button'));
+}
+
+const resetCarouselImageStyle = () => {
+    CAROUSEL.querySelectorAll('div').forEach(el => el.removeAttribute('class') & el.setAttribute('class', 'carousel-item'));
+}
+
+const classes = ['to-the-begining','to-the-left','to-the-end','to-the-fourth','to-the-fifth'];
+
+const allButtons = ['first-btn','second-btn','third-btn','fourth-btn','fifth-btn'];
 
 BURGER.addEventListener('click', (event) => {
     if (BURGER.classList.value == 'burger-collapse') {
@@ -48,48 +64,69 @@ WHOLE_PAGE.addEventListener('click', (event) => {
 });
 
 
+
 FIRST_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-end') & el.classList.add('to-the-left') & el.classList.remove('to-the-fourth') & el.classList.remove('to-the-fifth') & el.classList.add('to-the-begining'));
-    SECOND_BUTTON.classList.remove('clicked-btn');
-    THIRD_BUTTON.classList.remove('clicked-btn');
-    FOURTH_BUTTON.classList.remove('clicked-btn');
-    FIFTH_BUTTON.classList.remove('clicked-btn');
+    resetCarouselImageStyle();
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add('to-the-begining'));
+    resetButtonsStyle();
     FIRST_BUTTON.classList.add('clicked-btn');
+    image_index = 0;
 });
 
 SECOND_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-end') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-fourth') & el.classList.remove('to-the-fifth') & el.classList.add('to-the-left'));
-    THIRD_BUTTON.classList.remove('clicked-btn');
-    FIRST_BUTTON.classList.remove('clicked-btn');
-    FOURTH_BUTTON.classList.remove('clicked-btn');
-    FIFTH_BUTTON.classList.remove('clicked-btn');
+    resetCarouselImageStyle();
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add('to-the-left'));
+    resetButtonsStyle();
     SECOND_BUTTON.classList.add('clicked-btn');
+    image_index = 1;
 });
 
 
 THIRD_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-fourth') & el.classList.remove('to-the-fifth') & el.classList.add('to-the-end'));
-    SECOND_BUTTON.classList.remove('clicked-btn');
-    FIRST_BUTTON.classList.remove('clicked-btn');
-    FOURTH_BUTTON.classList.remove('clicked-btn');
-    FIFTH_BUTTON.classList.remove('clicked-btn');
+    resetCarouselImageStyle();
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add('to-the-end'));
+    resetButtonsStyle();
     THIRD_BUTTON.classList.add('clicked-btn');
+    image_index = 2;
 });
 
 FOURTH_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-end') & el.classList.remove('to-the-fifth') & el.classList.add('to-the-fourth'));
-    SECOND_BUTTON.classList.remove('clicked-btn');
-    FIRST_BUTTON.classList.remove('clicked-btn');
-    THIRD_BUTTON.classList.remove('clicked-btn');
-    FIFTH_BUTTON.classList.remove('clicked-btn');
+    resetCarouselImageStyle();
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add('to-the-fourth'));
+    resetButtonsStyle();
     FOURTH_BUTTON.classList.add('clicked-btn');
+    image_index = 3;
 });
 
 FIFTH_BUTTON.addEventListener('click', (event) => {
-    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.remove('to-the-left') & el.classList.remove('to-the-begining') & el.classList.remove('to-the-end') & el.classList.remove('to-the-fourth') & el.classList.add('to-the-fifth'));
-    SECOND_BUTTON.classList.remove('clicked-btn');
-    FIRST_BUTTON.classList.remove('clicked-btn');
-    THIRD_BUTTON.classList.remove('clicked-btn');
-    FOURTH_BUTTON.classList.remove('clicked-btn');
+    resetCarouselImageStyle();
+    CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add('to-the-fifth'));
+    resetButtonsStyle();
     FIFTH_BUTTON.classList.add('clicked-btn');
+    image_index = 4;
 });
+
+let classtoapply = '';
+let btnnumber = '';
+
+LEFT_CARRET.addEventListener('click', () => {
+    if (image_index > 0) {
+        image_index -= 1;
+        classtoapply = classes[image_index];
+        resetCarouselImageStyle();
+        CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add(classtoapply));
+        btnnumber = allButtons[image_index];
+        resetButtonsStyle();
+        document.getElementById(btnnumber).classList.add('clicked-btn');
+}});
+
+RIGHT_CARRET.addEventListener('click', () => {
+    if (image_index < 4) {
+        image_index += 1;
+        classtoapply = classes[image_index];
+        resetCarouselImageStyle();
+        CAROUSEL.querySelectorAll('div').forEach(el => el.classList.add(classtoapply));
+        btnnumber = allButtons[image_index];
+        resetButtonsStyle();
+        document.getElementById(btnnumber).classList.add('clicked-btn');
+}});
