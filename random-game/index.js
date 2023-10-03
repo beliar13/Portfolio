eb2fd6acf9041e4bc62be00f305756296cae7e62
Denis Sevostyanov <tesbeliar@gmail.com>
2023-10-03 21:28:00 +0300

    feat: add some functions

--- random-game/index.js
+++ random-game/index.js
@@ -1,1 +1,13 @@
-console.log('init random-game');
+const doors = document.querySelectorAll(".door");
+const targets = document.querySelectorAll(".target");
+const score = document.querySelector('score')
+
+const randomTime = (min, max) => {
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+const randomDoor = (doors) => {
+    
+    const nthdoor = Math.floor(Math.random()*doors.length)
+    console.log(nthdoor);
+}
